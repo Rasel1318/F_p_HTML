41e47a62b19db8c9afcf627333a716e267795096
Kazi Rasel <kazirasel2005@gmail.com>
2023-05-31 21:15:17 +0600

    commited Hello file

---
+++ hello.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <climits>
+#include <vector>
+#include <numeric>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <algorithm>
+using namespace std; 
+
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
+#define fr(i, x, n) for(int i = x; i<n; i++)
+#define ff first
+#define ss second
+#define ll long long
+
+
+int main(){
+    cout<<"Hello World!";
+    return 0;
+}
